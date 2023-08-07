4e751f38e98e198982f02e43226b88dc5aba876e
dmaddu <dmaddu@progress.com>
2023-08-07 15:24:46 +0530

    base commit
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,14 @@
-package cmd
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Responding from Fiber Microservice!!")
+	})
+
+	// Start server on port 3000
+	app.Listen(":3000")
+}
