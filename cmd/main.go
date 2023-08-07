4a0b5a4d09622900ec2def25e3a940f330b9c85b
dmaddu <dmaddu@progress.com>
2023-08-07 16:04:22 +0530

    base commit
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Dmaddu/microservice-repo/internal/app/middleware"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Responding from Fiber Microservice!!")
+	//app.Use(middleware.AuthenticateUser)
+	app.Get("/", middleware.AuthenticateUser, func(c *fiber.Ctx) error {
+		return c.SendString("Responding from Fiber Microservice!")
 	})
 
 	// Start server on port 3000
--- internal/app/middleware/authn_middleware.go
+++ internal/app/middleware/authn_middleware.go
@@ -1,1 +1,16 @@
-package handlers
+package middleware
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthenticateUser(c *fiber.Ctx) error {
+	// print current date and time to console
+	fmt.Println("Date:", time.Now(), " User Authenticated")
+
+	// passing the request to the next endpoint
+	return c.Next()
+}
